a40f687df5570727d4ea9a72e132354ca76be322
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2024-12-31 15:39:56 +0600

    issue_fix

--- lib/app/modules/video_call/views/video_call_view.dart
+++ lib/app/modules/video_call/views/video_call_view.dart
@@ -22,41 +22,51 @@ class VideoCallView extends GetView<VideoCallController> {
       body: Stack(
         children: [
           Obx(() {
-            return controller.userId.value != null
-                ? AgoraVideoView(
-                    controller: VideoViewController.remote(
-                      rtcEngine: controller.engine,
-                      canvas: VideoCanvas(uid: controller.userId.value),
-                      connection:
-                          RtcConnection(channelId: controller.channelName),
-                    ),
-                  )
-                : const Text(
-                    'Please wait for remote user to join',
-                    textAlign: TextAlign.center,
-                  );
+            return Center(
+              child: _remoteVideo(),
+            );
           }),
           Align(
             alignment: Alignment.topLeft,
             child: SizedBox(
               width: 100,
               height: 150,
-              child: Center(
-                child: Obx(() {
-                  return controller.localUserJoined.value
+              child: Obx(() {
+                return Center(
+                  child: controller.localUserJoined.value
                       ? AgoraVideoView(
                           controller: VideoViewController(
+                            useFlutterTexture: true,
                             rtcEngine: controller.engine,
                             canvas: const VideoCanvas(uid: 0),
                           ),
                         )
-                      : const CircularProgressIndicator();
-                }),
-              ),
+                      : const CircularProgressIndicator(),
+                );
+              }),
             ),
           ),
         ],
       ),
     );
+  }
+
+  // Widget to display remote video
+  Widget _remoteVideo() {
+    if (controller.userId.value != null) {
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          useFlutterTexture: true,
+          rtcEngine: controller.engine,
+          canvas: VideoCanvas(uid: controller.userId.value),
+          connection: RtcConnection(channelId: controller.channelName),
+        ),
+      );
+    } else {
+      return const Text(
+        'Please wait for remote user to join',
+        textAlign: TextAlign.center,
+      );
+    }
   }
 }
